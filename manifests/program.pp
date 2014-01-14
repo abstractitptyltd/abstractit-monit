1851822d06e4c8580d9b69a8d171a7320d9bc3ac
Pete Brown <rendhalver@gmail.com>
2014-01-14 11:26:17 +1000

    new define for monitoring a program

---
+++ manifests/program.pp
@@ -0,0 +1,51 @@
+# Define: monit::program
+#
+# add a program for monit to monitor
+#
+# Sample Usage:
+#
+define monit::program (
+  $program = '',
+  $path = '',
+  $start = '',
+  $stop = '',
+  $restarts = '5',
+  $cycles = '5',
+  $restart_action = 'timeout',
+  $status = '',
+  $status_tests = '3',
+  $status_cycles = '3',
+  $status_action = 'restart',
+  $host = '',
+  $port = '',
+  $protocol = '',
+  $type = '',
+  $host_tests = '3',
+  $host_cycles = '3',
+  $host_action = 'restart',
+  $template = 'monit/program.erb',
+) {
+
+  $real_program = $program ? {
+    ''      => $name,
+    default => $program,
+  }
+  $real_start = $start ? {
+    ''      => "/usr/sbin/service ${real_program} start",
+    default => $start,
+  }
+  $real_stop = $stop ? {
+    ''      => "/usr/sbin/service ${real_program} stop",
+    default => $stop,
+  }
+
+  file { "/etc/monit/conf.d/${name}.monitrc":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($template),
+    notify  => Service['monit'],
+  }
+
+}
