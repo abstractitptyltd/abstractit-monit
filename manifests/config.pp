f5197774f8443bc9f0579c162f402a4ef3d60ea1
Pete Brown <pete@abstractit.com.au>
2014-10-31 17:34:15 +1000

    remove old file fix config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,13 +4,11 @@
 #
 
 class monit::config (
-  $from_email = $monit::params::from_email,
-  $alert_email = $monit::params::alert_email,
-  $mailserver = $monit::params::mailserver,
+  $from_email  = '',
+  $alert_email = '',
+  $mailserver  = '',
   $check_local = $monit::params::check_local,
-  $check_disk = $monit::params::check_disk,
-) inherits monit::params {
-
+  $check_disk  = $monit::params::check_disk,) inherits monit::params {
   file { 'monitrc':
     ensure  => file,
     path    => '/etc/monit/monitrc',
