67cb6703bd59cf4dc0c6330c01a5c170134d12e2
Pete Brown <pete@abstractit.com.au>
2014-11-05 11:38:49 +1000

    additions to monit::process class

--- manifests/process.pp
+++ manifests/process.pp
@@ -6,29 +6,33 @@
 #
 
 define monit::process (
-  $ensure = present,
-  $process = '',
-  $service = '',
-  $start = '',
-  $stop = '',
-  $restarts = '5',
-  $cycles = '5',
+  $ensure         = present,
+  $process        = '',
+  $service        = '',
+  $timeout        = '',
+  $socket         = '',
+  $socket_timeout = '',
+  $start          = '',
+  $stop           = '',
+  $restarts       = '5',
+  $cycles         = '5',
   $restart_action = 'timeout',
-  $pidfile = '',
-  $host = '',
-  $port = '',
-  $protocol = '',
-  $type = '',
-  $host_tests = '3',
-  $host_cycles = '3',
-  $host_action = 'restart',
-  $template = 'monit/process.erb',
-) {
-
+  $pidfile        = '',
+  $host           = '',
+  $port           = '',
+  $protocol       = '',
+  $type           = '',
+  $socket_type    = '',
+  $groups         = [],
+  $depends        = [],
+  $host_tests     = '3',
+  $host_cycles    = '3',
+  $host_action    = 'restart',
+  $template       = 'monit/process.erb',) {
   include monit::install
   include monit::service
 
-  $real_ensure = $ensure ? {
+  $real_ensure  = $ensure ? {
     default  => file,
     true     => file,
     'absent' => 'absent',
@@ -41,17 +45,17 @@ define monit::process (
   }
   $real_service = $service ? {
     ''      => $name,
-    default => $service ,
+    default => $service,
   }
   $real_pidfile = $pidfile ? {
     ''      => "/var/run/${name}.pid",
     default => $pidfile,
   }
-  $real_start = $start ? {
+  $real_start   = $start ? {
     ''      => "/usr/sbin/service ${real_service} start",
     default => $start,
   }
-  $real_stop = $stop ? {
+  $real_stop    = $stop ? {
     ''      => "/usr/sbin/service ${real_service} stop",
     default => $stop,
   }
